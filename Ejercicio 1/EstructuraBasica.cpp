4a37cf6816173563a65cdd3040a513477f46c706
maritoo17 <msancgim@myuax.com>
2023-12-05 11:19:39 +0100

    Cambio de código debido a que el anterior no me iba porque no me detectaba la función variant.

--- Ejercicio 1/EstructuraBasica.cpp
+++ Ejercicio 1/EstructuraBasica.cpp
@@ -1,9 +1,6 @@
 #include <map>
 #include <string>
 #include <iostream>
-#include <variant>
-
-using namespace std;
 
 class Environment {
 public:
@@ -11,29 +8,13 @@ public:
 
     ~Environment() {}
 
-    using SymbolValue = variant<int, double, string>;
+    enum class ValueType {INT, DOUBLE, STRING};
+    struct SymbolValue {
+        ValueType type;
+        union {
+            int intValue;
+            double doubleValue;
+            std::string* stringValue;
+        };
+    };
 
-    void addSymbol(const string name, SymbolValue &value) {
-        symbolTable[name] = value;
-    }
-
-    SymbolValue getSymbolValue(const string &name) {
-        if (symbolTable.find(name) != symbolTable.end()) {
-            return symbolTable[name];
-        } else {
-            cerr << "Error: Symbol '" << name << "' no encontrado en el Environment." << endl;
-            return SymbolValue();
-        }
-    }
-
-private;
-    map<string, SymbolValue> symbolTable;
-};
-
-int main() {
-    Environment env;
-
-    env.addSymbol("x", 23);
-    env.addSymbol("y", 43.7);
-    env.addSymbol("nombre", "Juan Pablo");
-}
