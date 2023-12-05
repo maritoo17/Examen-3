7ab49ae363e17e1a223f9de76571557285e3441b
maritoo17 <msancgim@myuax.com>
2023-12-05 11:22:48 +0100

    Final de la clase Environment.

--- Ejercicio 1/EstructuraBasica.cpp
+++ Ejercicio 1/EstructuraBasica.cpp
@@ -16,5 +16,29 @@ public:
             double doubleValue;
             std::string* stringValue;
         };
+
+        SymbolValue() : type(ValueType::INT), intValue(0) {}
+        ~SymbolValue() {
+            if (type == ValueType::STRING) delete stringValue;
+        }
+
+        void addSymbol(const std::string& name, const SymbolValue& value) {
+            symbolTable[name] = value;
+        }
+
+        SymbolValue getSymbolValue(const std::string& name) {
+            if (symbolTable.find(name) != symbolTable.end()) {
+                return symbolTable[name];
+            } else {
+                std::cerr << "Error: Symbol '" << name << "' not found in the environment." << std::endl;
+                return SymbolValue();
+            }
+        }
+
+    private:
+        std::map<std::string, SymbolValue> symbolTable;
     };
+};
+
+
 
