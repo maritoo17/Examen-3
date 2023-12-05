85e76bb3135eac627641c2f8b17bee0c8a199e51
maritoo17 <msancgim@myuax.com>
2023-12-05 11:02:50 +0100

    Segunda parte del código en la que añado la librería que me faltaba y completo la clase Environment.

--- Ejercicio 1/EstructuraBasica.cpp
+++ Ejercicio 1/EstructuraBasica.cpp
@@ -1,5 +1,19 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <variant>
 
 using namespace std;
+
+class Environment {
+public:
+    Environment () {}
+
+    ~Environment () {}
+
+    using SymbolValue = variant<int , double, string>;
+
+    void addSymbol (const string name, SymbolValue& value) {
+        symbolTable[name] = value;
+    }
+};
